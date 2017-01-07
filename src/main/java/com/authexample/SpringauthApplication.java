e3ade300f13ddad6d66c847bd43c398505cbc3c5
Juan David Sánchez <juancho85@gmail.com>
2017-01-07 12:45:09 +0100

    remove unused imports

--- src/main/java/com/authexample/SpringauthApplication.java
+++ src/main/java/com/authexample/SpringauthApplication.java
@@ -5,17 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.AccessDeniedHandler;
+
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @SpringBootApplication
 public class SpringauthApplication {
