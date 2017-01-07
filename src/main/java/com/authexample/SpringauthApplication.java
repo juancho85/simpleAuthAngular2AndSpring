bbaa4524873347b63d1a357afd6d571330af550a
Juan David Sánchez <juancho85@gmail.com>
2017-01-07 13:29:23 +0100

    add spring default folders to serve static content with the generared ui

--- src/main/java/com/authexample/SpringauthApplication.java
+++ src/main/java/com/authexample/SpringauthApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
--- ui/src/app/app.routing.ts
+++ ui/src/app/app.routing.ts
@@ -6,7 +6,7 @@ import {SignUpComponent} from "./sign-up/sign-up.component";
 import {ProtectedComponent} from "./protected/protected.component";
 
 const APP_ROUTES: Routes = [
-    {path: '', redirectTo: '/signup', pathMatch: 'full'},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'signup', component: SignUpComponent},
     {path: 'login', component: SignInComponent},
     {path: 'protected', component: ProtectedComponent, canActivate: [AuthGuard]}
